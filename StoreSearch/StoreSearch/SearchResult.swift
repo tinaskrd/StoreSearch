237f77969a7c8ced4d56171b700d3c9f19de885f
Tina Skordaki <tinaskordaki@gmail.com>
2025-12-18 22:32:48 +0200

    added SearchResult class

---
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Tina  on 18.12.25.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -13,29 +13,49 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Variables
+    var searchResults = [String]()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right: 0)
     }
 }
 
 // MARK: - Search Bar Delegate
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("The search text is: \(searchBar.text!)")
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake result %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
+        searchBar.resignFirstResponder()
     }
 }
 
 // MARK: - Table View Delegate
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return searchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell() 
+        let cellIdentifier = "SearchResultCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell?.textLabel!.text = searchResults[indexPath.row]
+        return cell ?? UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
     }
-    
-    
+}
+
+// MARK: - Position
+extension SearchViewController {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
 }
